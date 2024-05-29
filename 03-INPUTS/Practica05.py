a9c28e6978034227ab8734453677af6979045366
loldl01 <lolitadavila94@gmail.com>
2024-05-28 22:57:26 -0500

    INPUTS

---
+++ 03-INPUTS/Practica05.py
@@ -0,0 +1,30 @@
+#Crear un codigo que permita ingresar una respuesta al usuario
+#haciendo la siguiente pregunta:
+#¿Qué estas estudiando?
+
+print(input("¿Que estas estudiando?: "))
+
+#crear un codigo que permita ingresar una respuesta al usuario
+#haciendo la siguiente pregunta y lo imprima:
+#¿En que pais vives?
+
+print(input("¿En que pais vives?: "))
+
+#Crear que muestre por pantalla la Provincia, Universidad y permite 
+#Ingresar la carrera de Estudio:
+#Formato ejm
+#Nombre de tu Provincia:
+#Nombre de tu Universidad:
+#Nombre de tu Carrera: 
+
+print("Tus datos son:" + input("Nombre de tu provincia: \n\t") + (input("Nombre de tu universidad: \n\t") + (input("Nombre de tu carrera: \n\t"))))
+
+
+#Vas a crear un código en Python que permita preguntar a tu 
+#amigo estas 2 preguntas:
+#¿Que ciudad te gustaria visitar?
+#¿Cual es tu color favorito?
+#y que luego le muestre en pantalla 
+#esas palabras combinadas, para formar una marca creativa.  
+
+print("mi marca creativa es: " + input("¿Que ciudad te gustaria visitar?: ") + (input("¿cual es tu color favorito?: ")))
