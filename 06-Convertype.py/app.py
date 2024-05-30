508a78cc8eea510eedfd31ea26301c298bb27e8c
loldl01 <lolitadavila94@gmail.com>
2024-05-29 21:28:08 -0500

    Convertype

---
+++ 06-Convertype.py/app.py
@@ -0,0 +1,10 @@
+edad = input("Dime tu edad: ")
+print(type(edad))
+edadconvertidad = int(edad)
+print(type(edadConvertidad))
+print("\nNueva edad")
+
+nuevaEdad = 1 + edadconvertidad
+print(type(nuevaEdad))
+EdadTexto = str(nuevaEdad)
+print(type(EdadTexto))
---
+++ 06-Convertype.py/practica01.py
@@ -0,0 +1,27 @@
+#Convierte el valor de num1 en un int e imprime el tipo de dato que resulta: 
+#num1 = "30"
+
+#num1 = "30"
+#print(type(num1))
+
+#num2 = int(num1)
+#print(type(num2))
+
+#num3 = "20.1"
+#print(type(num3))
+
+#num4 = float(num3)
+#print(type(num4))
+
+#Suma los valores de num1 y num2.
+#No modifiques el valor de las variables ya 
+#declaradas, sino aplica las conversiones necesarias 
+#dentro de la función print().
+
+num1 = "30"
+num2 = "2.3"
+
+total = num1 + num2 
+
+print(int(num1) +  float(num2))
+
