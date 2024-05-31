da1e7e5bd58cbf57aa008f3b548ff6b556df7590
loldl01 <lolitadavila94@gmail.com>
2024-05-30 22:06:26 -0500

    Propiedad

---
+++ 12.Propiedad/app.py
@@ -0,0 +1,22 @@
+#inmutables #los strings son inmutables
+nombre = "carina"
+#nombre[0] = "k"
+nombre = "Karina"
+#print(nombre)
+
+#concatenar
+n1 = "Kari"
+n2 = "na"
+#print(n1 + n2)
+
+#se puede multiplicar los strings
+#print(n1*3)
+
+#buscar
+poema = "Ecuador pais es uno de los mejores paraisos del mundo"
+#print("mejores" not in poema) #que busque la palabra mejores en la variable poema
+
+#valor boleano, puede ser verdadero o falso 
+
+#len es como saber el tamaño del string, es decir cuantas letras hay en esa variable
+print(len(poema))
---
+++ 12.Propiedad/practica01.py
@@ -0,0 +1,23 @@
+#Practica 1
+#Concatena 15 veces el texto "Repetición" y muestra 
+#el resultado en pantalla. Por suerte, conoces que los 
+#strings son multiplicables y puedes realizar esta actividad de forma simple y elegante.
+
+n1 = "repeticion"
+
+#print(n1*15)
+
+#Practica 2
+
+#Verifica si la palabra "agua" no se encuentra en el siguiente haiku. Debes imprimir el booleano. Tierra mojada, mis recuerdos de viaje entr
+
+frase = "Tierra mojada, mis recuerdos de viaje entre las lluvias"
+#print("agua" in frase)
+
+#Practica 3
+#Muestra en pantalla el largo (en números de caracteres) 
+#de la palabra 
+
+palabra = "electroencefalografista"
+print(len(palabra))
+
