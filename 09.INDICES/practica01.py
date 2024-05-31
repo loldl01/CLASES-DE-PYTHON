f126aec7624aaa6ca55847c0a1fb1362aaf6e3df
loldl01 <lolitadavila94@gmail.com>
2024-05-30 20:32:53 -0500

    INDICES

---
+++ 09.INDICES/practica01.py
@@ -0,0 +1,8 @@
+#PRACTICA 1
+#Encuentra y muestra en pantalla qué caracter
+#ocupa la quinta posición dentro de la siguiente palabra: "ordenador"
+
+palabra = "ordenador" 
+
+resultado = (palabra[5])  
+print(resultado)
---
+++ 09.INDICES/practica02.py
@@ -0,0 +1,8 @@
+#PRACTICA 2
+#Encuentra y muestra en pantalla el índice de la
+#primera aparición de la palabra "práctica" en la siguiente frase:
+#"En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+
+mi_texto = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+
+print(mi_texto.index("practica")) 
