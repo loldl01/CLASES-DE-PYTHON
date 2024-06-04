6bb9848995d31853b64316b53427d6c91b3aa8a8
loldl01 <lolitadavila94@gmail.com>
2024-06-03 22:16:53 -0500

    estructurascondicionales

---
+++ 21.estructurascondicionales/app.py
@@ -0,0 +1,6 @@
+#if 
+#elif = caso contrario, lo puede hacer como en cadena
+#indentacion es el espacio que se deja en la fila de abajo
+#else y elif son opcionales
+#diferencia entre elif y else
+#elif es otra condicion que podemos dar al codigo 
